a0ee525f05f67963e3554e0177bb0b5000ab2d43
MaryB16 <buda.maria89@gmail.com>
2018-06-13 18:44:02 +0300

    Beautified code

--- register_serviceWorker.js
+++ register_serviceWorker.js
@@ -2,12 +2,11 @@
 
 if ('serviceWorker' in navigator) {
 
-  navigator.serviceWorker.register('serviceWorker.js')
-    .then(function (registration) {
-      console.log('Service worker registration succeeded:', registration);
-    })
-    .catch(function (error) {
-      console.log('Service worker registration failed:', error);
-    });
+    navigator.serviceWorker.register('serviceWorker.js')
+        .then(function (registration) {
+            console.log('Service worker registration succeeded:', registration);
+        })
+        .catch(function (error) {
+            console.log('Service worker registration failed:', error);
+        });
 }
-
